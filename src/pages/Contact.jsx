f1cb2d9ec4b0d1a03fe6c831670adf16f63d35ef
Spandana <spandana1286@gmail.com>
2023-06-09 09:35:59 -0400

    contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,15 +1,11 @@
 import WomanImg from '../img/contact/1.png';
 
-import {
-  ImInstagram,
-  ImFacebook,
-  ImTwitter
-} from 'react-icons/im';
+
 
 export default function Contact() {
   return (
     <section className='section bg-white'>
-      <div className='container mx-auto h-full'>
+      <div className='container mx-auto h-full relative'>
         <div className='flex flex-col lg:flex-row h-full
           items-center justify-start pt-36 gap-x-8 text-center lg:text-left lg:mt-[-113px]'>
           <div className='lg:flex-1 lg:pt-24 px-4'>
@@ -17,23 +13,23 @@ export default function Contact() {
             <p className='mb-12 text-[22px] mt-[-10px] ml-[5px]'>
             Let's Capture Your Special Moments!!
             </p>
-            <form className='flex flex-col gap-y-4'>
+            <form action='https://getform.io/f/5eda4fa7-5f17-4933-aa64-849d69a55320' method='POST' className='flex flex-col gap-y-4' >
               <div className='flex gap-x-10'>
                 <input className='outline-none border-b border-b-primary h-[60px]
                   bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                  type='text' placeholder='Name'></input>
+                  type='text' name='name' placeholder='Name'></input>
                 <input className='outline-none border-b border-b-primary h-[60px]
                   bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                  type='text' placeholder='Email address'></input>
+                  type='text' name='email address' placeholder='Email address'></input>
 
               </div>
               <div className='flex gap-x-10'>
                 <input className='outline-none border-b border-b-primary h-[60px]
                   bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                  type='text' placeholder='Contact'></input>
+                  type='text' name='contact no' placeholder='Contact No.'></input>
                 <select
                   className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3'
-                  defaultValue='wedding'
+                  name='select' defaultValue=''
                 >
                   <option value='' disabled>Event type</option>
                   <option value='wedding'>Wedding</option>
@@ -46,12 +42,12 @@ export default function Contact() {
               </div>
               <input className='outline-none border-b border-b-primary h-[60px]
                   bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
-                type='text' placeholder='Event Details (if any)'></input>
+                type='text' name='event details' placeholder='Event Details (if any)'></input>
               <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>Send Message</button>
             </form>
             
           </div>
-          <div className='flex justify-end lg:w-1/2'>
+          <div className='flex justify-end lg:w-1/2 mt-[200px]'>
 
             <img src={WomanImg} alt='' />
 
